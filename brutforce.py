88309a68a98c8fa22b4a68eef7e7e0e44365f603
AlexianeBA <barbealexiane05@gmail.com>
2023-11-23 13:49:52 +0100

    finish brutforce

--- brutforce.py
+++ brutforce.py
@@ -22,15 +22,22 @@ def find_best_investment(actions, max_budget):
     best_investment = None
     best_return = 0.0
     print("entrer dans la méthode de trouvaille meilleur investissement")
+    # boucle qui parcourt toutes les actions
     for r in range(1, len(actions) + 1):
+        # génère toutes les combinaisons possible des éléments dans la liste des actions
         for subset in itertools.combinations(actions, r):
+            # calcul du cout total en additionnant les prix de chaque action
             total_price = sum(action["price"] for action in subset)
-            total_return = sum(action["profit"] * action["price"] for action in subset)
+            # calcul le rendement total en mutlipliant le profit par le prix pour chaque actions et additionne ces valeurs
+            total_return = sum(
+                (action["profit"] * action["price"]) / 100 for action in subset
+            )
 
             if total_price <= max_budget and total_return > best_return:
-                best_return = total_return
+                best_return = round(total_return, 2)
                 best_investment = subset
     print("fin boucle")
+    # retourne le meilleur investissement trouvé
     return best_investment, best_return
 
 
